09be5a1a627973a43be7c64b9f6fe1093083ff41
David Latorre <davidlatorre94@hotmail.com>
2022-11-18 11:25:00 -0500

    5.7 Implementacion Interceptor Token

---
+++ src/app/interceptores/token.interceptor.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { SeguridadService } from '../servicios/seguridad.service';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(public miServicioSeguridad: SeguridadService, 
+    private router: Router) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    
+    /** 
+     * Aquí preguntarmos si hay una sesión activa, es decir, 
+     * si ya el usuario está logeado.
+     * Con el fin de que si ya está logeado, 
+     * todas las peticiones que realice el usuario le ponemos el token.
+    */ 
+    if(this.miServicioSeguridad.usuarioSesionActiva){
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.miServicioSeguridad.usuarioSesionActiva.token}`
+        }
+      });
+    }
+
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if(err.status == 401) {
+          this.router.navigateByUrl('/pages/dashboard');
+        }
+        return throwError(err);
+      })
+    );
+
+  }
+
+}
